8118f8b12ac858223ce5b21d44cde694be0ba164
kota <s1811539s.tsukuba.ac.jp>
2020-07-24 14:32:58 +0900

    finish ch6

--- memo2/ViewController.swift
+++ memo2/ViewController.swift
@@ -11,7 +11,7 @@ private let unselectedRow = -1
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
-
+    
     @IBOutlet weak var editMemoField: UITextField!
     @IBOutlet weak var memoListView: UITableView!
     var memoList: [String] = []
@@ -20,55 +20,67 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         memoListView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        editMemoField.becomeFirstResponder()
+        
+        let defaults = UserDefaults.standard
+        let loadedMemoList = defaults.object(forKey: "MEMO_LIST")
+        if (loadedMemoList as? [String] != nil) {
+            memoList = loadedMemoList as! [String]
+        }
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
+    
     @IBAction func tapSubmitButton(_ sender: Any) {
         applyMemo()
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memoList.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
-           if indexPath.row >= memoList.count {
-               return cell
-           }
-
-           cell.textLabel?.text = memoList[indexPath.row]
-           return cell
-       }
-
+        if indexPath.row >= memoList.count {
+            return cell
+        }
+        
+        cell.textLabel?.text = memoList[indexPath.row]
+        return cell
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           if indexPath.row >= memoList.count {
-               return
-           }
-           editRow = indexPath.row
-           editMemoField.text = memoList[editRow]
-       }
-       
+        if indexPath.row >= memoList.count {
+            return
+        }
+        editRow = indexPath.row
+        editMemoField.text = memoList[editRow]
+    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-           applyMemo()
-           return true
-       }
-       
+        applyMemo()
+        return true
+    }
+    
     func applyMemo() {
-           if editMemoField.text == nil {
-               return
-           }
-           
-           if editRow == unselectedRow {
-               memoList.append(editMemoField.text!)
-           } else {
-               memoList[editRow] = editMemoField.text!
-           }
-           editMemoField.text = ""
-           editRow = unselectedRow
-           memoListView.reloadData()
-       }
+        if editMemoField.text == nil {
+            return
+        }
+        
+        if editRow == unselectedRow {
+            memoList.append(editMemoField.text!)
+        } else {
+            memoList[editRow] = editMemoField.text!
+        }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(memoList, forKey: "MEMO_LIST")
+        
+        editMemoField.text = ""
+        editRow = unselectedRow
+        memoListView.reloadData()
+    }
 }
 
