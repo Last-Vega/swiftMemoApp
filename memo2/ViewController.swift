8db872c7e03791bc1c504d9183210afebe5cbffb
yugo sato <s1811494@klis.tsukuba.ac.jp>
2020-07-24 10:00:03 +0900

    create memo2

---
+++ memo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo2
+//
+//  Created by 佐藤祐吾 on 2020/07/24.
+//  Copyright © 2020 佐藤祐吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
